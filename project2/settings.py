dd24c2de6c41d6489e6a5d4bb4afd3d556471357
BoyaGangaraju <boyagangaraju53@gmail.com>
2025-12-24 16:07:02 +0530

    pro322

--- project2/settings.py
+++ project2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mg2$3p=(wazb%!f&akiakao0s!&+jsu#vk4i9^tr9p^@-ok9rb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-project2-uaxp.onrender.com","127.0.0.1","localhost","127.0.0.1:8000"]
 
 
 # Application definition
